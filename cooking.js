dc35b681eb4b156f30d30f4c5efe2910cdf570b0
Sujan Rana <khembikram1@gmail.com>
2023-06-17 18:36:22 +0545

    init Commit

---
+++ cooking.js
@@ -0,0 +1,8 @@
+function myfunction() {
+    var x = document.getElementById("mylinks");
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
